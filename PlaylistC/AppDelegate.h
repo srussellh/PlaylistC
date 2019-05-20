eef89c4a2aa2779c9f0bf41037c4ea7c24a7d8ed
srussellh <shanerherbert@gmail.com>
2019-05-20 10:13:05 -0600

    Initial Commit

---
+++ PlaylistC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlaylistC
+//
+//  Created by Bobba Kadush on 5/20/19.
+//  Copyright © 2019 Bobba Kadush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlaylistC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlaylistC
+//
+//  Created by Bobba Kadush on 5/20/19.
+//  Copyright © 2019 Bobba Kadush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
